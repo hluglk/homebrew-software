a99a3bde41e6a3024671a689f36fe9ab07109752
hluglk <hluglk@MacBook-Pro.local>
2026-06-08 20:00:30 +0800

    fix(cask): use dynamic version in siyuan-unlock url

--- Casks/siyuan-unlock.rb
+++ Casks/siyuan-unlock.rb
@@ -1,7 +1,7 @@
 cask "siyuan-unlock" do
   version "3.6.5"
 
-  url "https://github.com/appdev/siyuan-unlock/releases/download/v#{version}/siyuan-3.6.5-mac-arm64.dmg"
+  url "https://github.com/appdev/siyuan-unlock/releases/download/v#{version}/siyuan-#{version}-mac-arm64.dmg"
   name "siyuan-unlock"
   desc "思源笔记免登录版本；可以不登录使用同步功能"
   homepage "https://github.com/appdev/siyuan-unlock"
