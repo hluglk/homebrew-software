c2ef2d477ff2e4fe97d1599c0eeb4bfe0b1e6acf
hluglk <641766459@qq.com>
2026-06-10 14:46:44 +0800

    Update siyuan-unlock.rb

--- Casks/siyuan-unlock.rb
+++ Casks/siyuan-unlock.rb
@@ -1,18 +1,22 @@
 cask "siyuan-unlock" do
-  version "3.6.5"
-  sha256 "0d842ddc77adecb443ab423d8db530c028be475734f94438420199ed4605cefc"
+  arch arm: "-arm64"
+  version "3.6.4"
+  sha256 arm:   "a1b2c3d4...",
+         intel: "e5f6g7h8..."
 
-  url "https://github.com/appdev/siyuan-unlock/releases/download/v#{version}/siyuan-#{version}-mac-arm64.dmg"
+  url "https://github.com/appdev/siyuan-unlock/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "siyuan-unlock"
   desc "思源笔记免登录版本；可以不登录使用同步功能"
   homepage "https://github.com/appdev/siyuan-unlock"
 
-  livecheck do
-    url :homepage
-    strategy :github_latest
-  end
-
-  depends_on :macos
+  depends_on macos: :big_sur
 
   app "SiYuan.app"
+
+  zap trash: [
+    "~/.siyuan",
+    "~/Library/Application Support/SiYuan",
+    "~/Library/Preferences/org.b3log.siyuan.plist",
+    "~/Library/Saved Application State/org.b3log.siyuan.savedState",
+  ]
 end
