b053acc669d2afb7208d8b779f58b568c538f8c1
Anka <runner@sat12-dp149-64d2a3eb-735f-4c4c-ada6-4295d304af4b-7E07E84AE262.local>
2026-09-23 15:57:21 +0000

    siyuan-unlock 3.8.5

--- Casks/siyuan-unlock.rb
+++ Casks/siyuan-unlock.rb
@@ -1,9 +1,9 @@
 cask "siyuan-unlock" do
   arch arm: "-arm64"
 
-  version "3.8.3"
-  sha256 arm:   "89706fc50f9d6cb9eef61f8ebd5ae336d160f11ba41ba654882f61a660017036",
-         intel: "5465ac5f207beb2a67eb3501e300a1a671604c3d1996d45b1ac06dc5156001c8"
+  version "3.8.5"
+  sha256 arm:   "9393deb24e79fd750d830d342b9ea754120f35fc3da8dab37495b1c212172644",
+         intel: "a365711f6aaee67a989064b60161ac5c337034a656796117fe896b64d3404c94"
 
   url "https://github.com/appdev/siyuan-unlock/releases/download/v#{version}/siyuan-#{version}-mac#{arch}.dmg"
   name "siyuan-unlock"
